1434c2cfd808dc926d2e873049f0c2b89fee49ce
Amr Adel <amr96.adel@gmail.com>
2024-05-06 21:57:49 +0300

    TASK 00 - DOCM

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,14 @@ import random
 
 
 async def wait_random(max_delay=10):
+    """_summary_
+
+    Args:
+        max_delay (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        _type_: _description_
+    """
     n = random.uniform(0, max_delay)
     await asyncio.sleep(n)
     return n
